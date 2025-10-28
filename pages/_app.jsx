f9b10ee8831999fc3c961265ebaf28d912ea2142
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-10-28 04:52:54 -0300

    Implement internationalization across multiple pages
    - Integrate next-i18next for translation support in the application.
    - Update AppHeader, About, Contact, Home, and Project pages to utilize translation keys.
    - Ensure server-side translations are fetched for dynamic content rendering.
    - Refactor components to improve readability and maintainability.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,21 @@
-import '../styles/globals.css';
-import { AnimatePresence } from 'framer-motion';
-import DefaultLayout from '../components/layout/DefaultLayout';
-import UseScrollToTop from '../hooks/useScrollToTop';
+import "../styles/globals.css";
+import { AnimatePresence } from "framer-motion";
+import DefaultLayout from "../components/layout/DefaultLayout";
+import UseScrollToTop from "../hooks/useScrollToTop";
+import { appWithTranslation } from "next-i18next";
+import nextI18NextConfig from "../next-i18next.config";
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<AnimatePresence>
-			<div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
-				<DefaultLayout>
-					<Component {...pageProps} />
-				</DefaultLayout>
-				<UseScrollToTop />
-			</div>
-		</AnimatePresence>
-	);
+  return (
+    <AnimatePresence>
+      <div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
+        <DefaultLayout>
+          <Component {...pageProps} />
+        </DefaultLayout>
+        <UseScrollToTop />
+      </div>
+    </AnimatePresence>
+  );
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp, nextI18NextConfig);
--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,14 +1,23 @@
-import PagesMetaHead from '../../components/PagesMetaHead';
-import ProjectsGrid from '../../components/projects/ProjectsGrid';
+import PagesMetaHead from "../../components/PagesMetaHead";
+import ProjectsGrid from "../../components/projects/ProjectsGrid";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 function index() {
-	return (
-		<div className="container mx-auto">
-			<PagesMetaHead title="Projects" />
+  return (
+    <div className="container mx-auto">
+      <PagesMetaHead title="Projects" />
 
-			<ProjectsGrid />
-		</div>
-	);
+      <ProjectsGrid />
+    </div>
+  );
 }
 
 export default index;
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+}
