5447bb15547f6bf84c4f4490ddf53ab80ecc389a
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-11-15 07:53:25 -0300

    Update PlayButton component label and icon prop for enhanced customization

--- components/reusable/PlayButton.jsx
+++ components/reusable/PlayButton.jsx
@@ -3,9 +3,10 @@
 import { Play } from "lucide-react";
 
 export function PlayButton({
-  label = "Confira o projeto",
+  label = "Confira o repositório de Código",
   onClick,
   className = "",
+  icon = <Play size={18} />,
 }) {
   return (
     <button
@@ -13,7 +14,7 @@ export function PlayButton({
       className={`flex items-center gap-2 text-md font-general-medium min-w-24 bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-md px-5 py-2.5 duration-300 ${className}`}
       aria-label={label}
     >
-      <Play size={18} />
+      {icon}
       {label}
     </button>
   );
